78233db32f55933b985c05b63813776279bd44d2
EGIvanyshchenko <e.g.ivanyshchenko@gmail.com>
2024-01-08 20:24:12 +0200

    Ваш комментарий к коммиту

---
+++ JS/2_Add_Two_Numbers/dist/index.js
@@ -0,0 +1,29 @@
+class ListNode {
+    constructor(val = 0, next = null) {
+        this.val = val;
+        this.next = next;
+    }
+}
+function addTwoNumbers(l1, l2) {
+    let dummyHead = new ListNode(); // Dummy head to simplify the code
+    let current = dummyHead;
+    let carry = 0;
+    while (l1 !== null || l2 !== null || carry !== 0) {
+        const x = l1 ? l1.val : 0;
+        const y = l2 ? l2.val : 0;
+        const sum = x + y + carry;
+        carry = Math.floor(sum / 10);
+        current.next = new ListNode(sum % 10);
+        current = current.next;
+        if (l1 !== null)
+            l1 = l1.next;
+        if (l2 !== null)
+            l2 = l2.next;
+    }
+    return dummyHead.next;
+}
+// Example usage:
+let l1 = new ListNode(2, new ListNode(4, new ListNode(3)));
+let l2 = new ListNode(5, new ListNode(6, new ListNode(4)));
+let result = addTwoNumbers(l1, l2);
+console.log(result);
